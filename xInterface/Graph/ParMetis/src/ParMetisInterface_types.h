916dc7c6f922fd49b8812d43d70fec443826c224
Nicolas Chevaugeon <nicolas.chevaugeon@ec-nantes.fr>
2021-11-25 11:38:03 +0100

    force added ParMetisInterface_types.h ... should be cleaned up later

---
+++ xInterface/Graph/ParMetis/src/ParMetisInterface_types.h
@@ -0,0 +1,6 @@
+/*
+be careful this type should depends on Metis and parmetis actual compilations options.
+I leave like that for now.
+*/
+typedef int parmetis_indx_t;
+typedef float parmetis_real_t; 
